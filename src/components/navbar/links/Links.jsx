0c27b5eb7636fa6324be850279d3b57f48f897bf
souravkumar <souravkumarswain96@gmail.com>
2026-09-04 22:37:39 +0530

    style with media queries

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,5 +1,6 @@
+"use client"
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './links.module.css'
 import NavLink from './navLinks/NavLink'
 
@@ -18,18 +19,47 @@ const Links = () => {
         path: '/contact'
     },
     {
-        title: "Admin",
-        path: '/admin'
-    },
-    {
         title: "Blog",
         path: '/blog'
     }]
+
+    const [open,setOpen] = useState(false)
+
+    let sessions = true;
+    let admin = true;
     return (
+        <div>
         <div className={styles.links}>
             {
                 headerLinks.map((eachLink) => <NavLink key = {eachLink.title} title={eachLink.title} path={eachLink.path}/>)
             }
+            {
+                sessions ? 
+                (
+                <>
+                    {admin && <NavLink  path="/admin" title="Admin"/>}
+                    <button className={styles.logout}>Logout</button>
+                </>
+                ) : 
+                (
+                    <NavLink path="/login" title="Login"/>
+                )
+            }
+        </div>
+        <button className={styles.menuButton} onClick={() => setOpen((prev) => !prev)}>Menu</button>
+        {
+            open && (
+                <div className={styles.mobileLinks}>
+                    {
+                        headerLinks.map(eachLink => 
+                        <NavLink key = {eachLink.title}
+                            path={eachLink.path}
+                            title = {eachLink.title}
+                        />)
+                    }
+                </div>
+            )
+        }
         </div>
     )
 }
