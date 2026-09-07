4c3d4e23d698c9e0d79e063995bca9254f2f2b1c
souravkumar <souravkumarswain96@gmail.com>
2026-09-07 11:43:12 +0530

    Homepage designed

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,12 @@
+import Image from "next/image";
+import styles from './about.module.css'
+
 const AboutPage = () => {
    return(
      <div>
-        This is about page
+        <div className={styles.imgContainer}>
+         <Image src='/about.png' alt="about_img" width={300} height={300}/>
+        </div>
     </div>
    )
 }
---
+++ src/app/page.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import styles from './home.module.css'
+import Image from 'next/image';
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+          <h1 className={styles.title}>Creative Thought Agency</h1>
+          <p className={styles.desc}>Lorem, ipsum dolar sit amet
+            consectetur elit. Vero blanditid adipsci minima reiciendia a 
+            autum assumenda dolore.
+          </p>
+          <div className={styles.button}>
+              <button className={styles.button}>Learn More</button>
+              <button className={styles.button}>Contact</button>
+          </div>
+          <div className={styles.brands}>
+            <Image src='/brands.png' alt="brands" width={500} height={50}/>
+          </div>
+      </div>
+      <div className={styles.imgContainer}>
+          <Image src='/hero.gif' alt="hero" width={350} height={350}/>
+      </div>
+    </div>
+  )
+}
+
+export default Home;
