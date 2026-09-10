88c68e436b5aed2807c24be121c19fe42b400482
souravkumar <souravkumarswain96@gmail.com>
2026-09-10 10:44:10 +0530

    Contact page added

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,27 @@
+import Image from 'next/image';
+import styles from './contact.module.css'
+
 const ContactPage = () => {
    return(
-     <div>
-        This is Contact page
+     <div className={styles.container}>
+       <div className={styles.imgContainer}>
+         <Image
+            src='/contact.png'
+            alt="contact img"
+            fill
+            className={styles.img}
+         />
+       </div>
+       <div className={styles.formContainer}>
+         <form action="" className={styles.form}>
+            <input type="text" placeholder='Name and Surname' />
+            <input type="text" placeholder='Email Address'/>
+            <input type="text" placeholder='Phone Number (Optional)'/>
+            <textarea name="" id="" cols="30" rows="10"
+            placeholder='message'></textarea>
+            <button>Send</button>
+         </form>
+       </div>
     </div>
    )
 }
